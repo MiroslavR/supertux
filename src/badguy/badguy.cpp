23de27dd3592e9e1fe8aa3f4dacc7a8f58bb0115
Hume2 <teratux.mail@gmail.com>
2015-08-17 07:58:46 +0200

    Changed "}else{" to "} else {"

--- src/badguy/badguy.cpp
+++ src/badguy/badguy.cpp
@@ -271,7 +271,7 @@ BadGuy::save(lisp::Writer& writer) {
   MovingSprite::save(writer);
   if(dir == LEFT){
     writer.write("direction", "left", false);
-  }else{
+  } else {
     writer.write("direction", "right", false);
   }
   if(dead_script != ""){
@@ -611,7 +611,7 @@ BadGuy::is_offscreen() const
       Camera *cam = Sector::current()->camera;
       dist = cam->get_center() - get_bbox().get_middle();
     }
-  }else{
+  } else {
     Player* player = get_nearest_player();
     if (!player) return false;
     dist = player->get_bbox().get_middle() - bbox.get_middle();
